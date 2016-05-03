df9b7cc1d62fe91a0714e0748d2f14a4c63689ee
ebenpack <ebenpackwood@gmail.com>
2016-05-03 19:33:30 -0400

    Add IO (dis)connect methods

--- src/main/scala/patches/Node/Node.scala
+++ src/main/scala/patches/Node/Node.scala
@@ -1,28 +1,42 @@
 package patches.Node
 
+import monifu.concurrent.Implicits.globalScheduler
 import monifu.concurrent.cancelables.BooleanCancelable
+import monifu.reactive.Ack
+import monifu.reactive.Ack.Continue
 import patches.IO.{Input, Message, Output}
+
+import scala.concurrent.Future
 
 abstract class Node(val name: String) {
   val inputs: List[Input[Message]]
   val outputs: List[Output[Message]]
-  //private var disconnects = Map[(Input[Message], Output[Message]), BooleanCancelable]()
+  private var disconnects = Map[(Input[_], Output[_]), BooleanCancelable]()
 
   def value: String
 
-//  def connect(i: Input[Message], o: Output[Message], topic: String) = (i, o) match {
-//    case (in, out) if in.canConnect(out) => disconnects = disconnects +
-//      ((in, out) -> out.out.connect())
-//    case _ => ;
-//  }
-//
-//  def disconnect(i: Input[Message], o: Output[Message], topic: String) = {
-//    disconnects.get((i, o)).map(_.cancel())
-//    disconnects = disconnects - ((i, o))
-//  }
-//
-//  def disconnectAll() = {
-//    for ((io, c) <- disconnects) c.cancel()
-//    disconnects = Map[(Input[Message], Output[Message]), BooleanCancelable]()
-//  }
+  def connect(i: Input[Message], o: Output[Message]) = (i, o) match {
+    case (in, out) if in.canConnect(out) && !disconnects.contains((in, out)) => {
+      println("CONNECTIN!")
+      val cancel = out.out.subscribe(f => {
+        println("ACK", f)
+        in.update(f)
+        Continue
+      })
+      disconnects = disconnects +
+        ((in, out) -> cancel)
+    }
+    case _ => ;
+  }
+
+  def disconnect(i: Input[Message], o: Output[Message]) = {
+    disconnects.get((i, o)).map(_.cancel())
+    disconnects = disconnects - ((i, o))
+  }
+
+  def disconnectAll() = {
+    for ((_, c) <- disconnects)
+      c.cancel()
+    disconnects = disconnects.empty
+  }
 }
