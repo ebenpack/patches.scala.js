7a2fda0c404b8f225ad5dbc50162bc846c40f71a
ebenpack <ebenpackwood@gmail.com>
2016-05-15 07:25:22 -0400

    Update monix

--- src/main/scala/patches/IO/Input.scala
+++ src/main/scala/patches/IO/Input.scala
@@ -1,22 +1,21 @@
 package patches.IO
 
-import monifu.reactive.OverflowStrategy
-import monifu.reactive.channels.{BehaviorChannel, SubjectChannel}
-import monifu.concurrent.Implicits.globalScheduler
-import monifu.reactive.subjects.PublishSubject
+import monix.execution.Scheduler.Implicits.global
+import monix.reactive.OverflowStrategy.DropOld
+import monix.reactive.subjects.ConcurrentSubject
 
 import scala.reflect.ClassTag
 
 class Input[T](name: String, initial: T)(implicit val tag: ClassTag[T]) {
 
-  private val input = BehaviorChannel[T](initial, OverflowStrategy.DropOld(100))
+
+  private val input = ConcurrentSubject.behavior(initial, DropOld(100))
   val in = input.behavior(initial)
   in.connect()
-
   def canConnect(o: Output[_]) = o.tag == tag
 
-  def update(value: T) =
-    input.pushNext(value)
+  def update(value: T) = input.onNext(value)
+
 }
 
 object Input {
--- src/main/scala/patches/IO/Output.scala
+++ src/main/scala/patches/IO/Output.scala
@@ -1,17 +1,17 @@
 package patches.IO
 
-import monifu.concurrent.Implicits.globalScheduler
-import monifu.reactive.OverflowStrategy
-import monifu.reactive.channels.{BehaviorChannel, PublishChannel}
+import monix.execution.Scheduler.Implicits.global
+import monix.reactive.OverflowStrategy.DropOld
+import monix.reactive.subjects.ConcurrentSubject
 
 import scala.reflect.ClassTag
 
 class Output[T](name: String, initial: T)(implicit val tag: ClassTag[T]) {
-  private val output = BehaviorChannel[T](initial, OverflowStrategy.DropOld(100))
+
+  private val output = ConcurrentSubject.behavior(initial, DropOld(100))
   val out = output.behavior(initial)
   out.connect()
-
-  def update(value: T) = output.pushNext(value)
+  def update(value: T) = output.onNext(value)
 }
 
 object Output {
