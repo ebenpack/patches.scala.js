1c2986832274544b18d4a5597f71a3dfde15bb08
ebenpack <ebenpackwood@gmail.com>
2016-05-22 18:18:37 -0400

    Update canvas rendering

--- src/main/scala/patches/Draw/Canvas.scala
+++ src/main/scala/patches/Draw/Canvas.scala
@@ -1,40 +1,27 @@
 package patches.Draw
 
-import patches.Node.{Node => N}
+import diode.react.ModelProxy
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.prefix_<^._
-import japgolly.scalajs.react.extra.OnUnmount
-import patches.Node.Math.Sum
 
 object Canvas {
 
-  case class State(nodes: List[N])
-
-  class Backend($: BackendScope[Unit, State]) extends OnUnmount {
-
-    def addNode(e: ReactMouseEvent) =
-      $.modState(s => s.copy(Sum() :: s.nodes))
-
-    def render(s: State) = {
+  val canvas = ReactComponentB[ModelProxy[CanvasModel]]("Canvas")
+    .render_P(proxy => {
+      val dispatch = proxy.dispatch
+      val controls = proxy().controls
       <.div(
-        <.div(
-          s.nodes.map(n =>
-            patches.Draw.Node(patches.Draw.Node.Props(n))()
-          ).toJsArray
-        ),
-        <.button(
-          ^.onMouseDown ==> addNode,
-          "Add Node"
-        )
+        ^.className := "canvas",
+        proxy().nodes.map(n => patches.Draw.Node(
+          dispatch,
+          n
+        )),
+        Controls(dispatch, proxy().controls)
       )
     }
-  }
-
-  val component = ReactComponentB[Unit]("Canvas")
-    .initialState[State](State(List[N]()))
-    .renderBackend[Backend]
+    )
     .build
 
-  def apply() =
-    component
+  def apply(p: ModelProxy[CanvasModel]) =
+    canvas(p)
 }
