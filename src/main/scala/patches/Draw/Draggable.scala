b95d6b3b0ef4ed248e8a52d198ab2efc5de71a48
ebenpack <ebenpackwood@gmail.com>
2016-05-22 18:19:11 -0400

    Factor out draggable trait

---
+++ src/main/scala/patches/Draw/Draggable.scala
@@ -0,0 +1,40 @@
+package patches.Draw
+
+import monix.reactive.MulticastStrategy
+import monix.reactive.OverflowStrategy.DropOld
+import monix.reactive.subjects.ConcurrentSubject
+import monix.execution.Scheduler.Implicits.global
+
+trait Draggable {
+  def getXY(): (Double, Double)
+
+  def onMove(x: Double, y: Double): Unit
+
+  class Position(val x: Double, val y: Double)
+
+  class Mouse(
+               val clientX: Double,
+               val clientY: Double,
+               val eventType: String
+             )
+
+  val mouseDownChannel = ConcurrentSubject[Mouse](MulticastStrategy.publish, DropOld(100))
+  val mouseMoveChannel = ConcurrentSubject[Mouse](MulticastStrategy.publish, DropOld(100))
+
+  mouseDownChannel
+    .flatMap(downEvent => {
+      val (startPosX, startPosY) = getXY()
+      val startMouseX = downEvent.clientX
+      val startMouseY = downEvent.clientY
+      mouseMoveChannel
+        .takeWhile(
+          _.eventType == "mousemove"
+        )
+        .map(m => {
+          val deltaX = m.clientX - startMouseX
+          val deltaY = m.clientY - startMouseY
+          new Mouse(startPosX + deltaX, startPosY + deltaY, m.eventType)
+        })
+    })
+    .foreach(m => onMove(m.clientX, m.clientY))
+}
