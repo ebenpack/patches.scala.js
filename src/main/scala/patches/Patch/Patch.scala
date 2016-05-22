727ce765729669c131f3d97c3ef29d4071aeb901
ebenpack <ebenpackwood@gmail.com>
2016-05-22 18:22:48 -0400

    Add Patch class

---
+++ src/main/scala/patches/Patch/Patch.scala
@@ -0,0 +1,20 @@
+package patches.Patch
+
+import monix.execution.Cancelable
+import monix.execution.Scheduler.Implicits.global
+import patches.IO.{Input, Message, Output}
+import patches.Node.Node
+
+class Patch {
+  var cancel: Option[Cancelable] = None
+  def connect(i: Input[Message], o: Output[Message], inputNode: Node) = (i, o) match {
+    case (in, out) if in.canConnect(out) => {
+      cancel = Some(out.out.subscribe(in.in))
+    }
+    case _ => ;
+  }
+
+  def disconnect(i: Input[Message], o: Output[Message]) =
+    cancel.foreach(_.cancel())
+
+}
