3fc7efef3fef8858cc19e36b511d0582c1b43988
ebenpack <ebenpackwood@gmail.com>
2016-08-07 09:41:33 -0400

    Update example

--- src/main/scala/patches/ScalaJSExample.scala
+++ src/main/scala/patches/ScalaJSExample.scala
@@ -1,44 +1,18 @@
 package patches
 
-
-import japgolly.scalajs.react.ReactDOM
-import org.scalajs.dom
-
-import scala.scalajs.js
-import jsactor.logging.impl.JsNullActorLoggerFactory
-import jsactor.{JsActor, JsActorSystem, JsProps}
-import org.scalajs.dom
-import patches.Draw.{AppCircuit, Canvas => DrawCanvas}
-import patches.Canvas.{Canvas => C}
-import patches.IO.Messages._
-import patches.Node.Math.Sum
+import patches.Actor.AkkaConfig.config
+import akka.actor._
 
 import scala.scalajs.js
 
-object ScalaJSExample extends js.JSApp {
-
-  case class Mess(int: String)
-
-  class Listener() extends JsActor {
-    def receive = {
-      case n: AddNode => println("listnmess", n)
-      case n: MoveNode => println("listndouble", n)
-    }
-  }
-
-  object Listener {
-    def apply(): Listener = new Listener()
-  }
+object Patches extends js.JSApp {
 
   def main(): Unit = {
-    val d = dom.document.getElementById("playground")
-    val c = new C
-    val g = Sum.props()
-
-    val p = AppCircuit.connect(a => a)(p => DrawCanvas(p))
-    ReactDOM.render(
-      p,
-      dom.document.getElementById("playground")
+    val system = ActorSystem("akkajsapp", config)
+    val bod = system.actorOf(
+      Props(Canvas("playground")),
+      "body"
     )
   }
+
 }
